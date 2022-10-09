2e5231660de98d14540a30f9ef7574dcbce95c0b
Field Palmer <gfieldpalmer@gmail.com>
2022-10-09 09:22:00 -0600

    yes

---
+++ responsive-web-app/src/constants/index.js
@@ -0,0 +1,207 @@
+import {
+   avatar,
+   facebook,
+   instagram,
+   linkedin,
+   twitter,
+   airbnb,
+   binance,
+   coinbase,
+   dropbox,
+   send,
+   shield,
+   star
+} from '../assets';
+
+export const navLinks = [
+   {
+      id: 'home',
+      title: 'Home'
+   },
+   {
+      id: 'features',
+      title: 'Features'
+   },
+   {
+      id: 'product',
+      title: 'Product'
+   },
+   {
+      id: 'clients',
+      title: 'Clients'
+   }
+];
+
+export const features = [
+   {
+      id: 'feature-1',
+      icon: star,
+      title: 'Creative',
+      content: 'Lorem ipsum dolor sit amet amincus salve armour corridor raxen forza inter brava'
+   },
+   {
+      id: 'feature-2',
+      icon: shield,
+      title: 'Unique',
+      content: 'Lorem ipsum dolor sit amet amincus salve armour corridor raxen forza inter brava'
+   },
+   {
+      id: 'feature-3',
+      icon: send,
+      title: 'Efficient',
+      content: 'Lorem ipsum dolor sit amet amincus salve armour corridor raxen forza inter brava'
+   }
+];
+
+export const feedback = [
+   {
+      id: 'feedback-1',
+      content:
+         'Lorem ipsum dolor sit amet, consectetur  elit, sed do eiusmod tempor ut labore et dolore magna aliqua.',
+      name: 'Client Name',
+      title: 'Reviewer Name',
+      img: avatar
+   },
+   {
+      id: 'feedback-2',
+      content:
+         'Lorem ipsum dolor sit amet, consectetur  elit, sed do eiusmod tempor ut labore et dolore magna aliqua.',
+      name: 'Client Name',
+      title: 'Reviewer Name',
+      img: avatar
+   },
+   {
+      id: 'feedback-3',
+      content:
+         'Lorem ipsum dolor sit amet, consectetur  elit, sed do eiusmod tempor ut labore et dolore magna aliqua.',
+      name: 'Client Name',
+      title: 'Reviewer Name',
+      img: avatar
+   }
+];
+
+export const stats = [
+   {
+      id: 'stats-1',
+      title: 'Users Active',
+      value: '0000+'
+   },
+   {
+      id: 'stats-2',
+      title: 'Clients who Trust Us',
+      value: '00+'
+   },
+   {
+      id: 'stats-3',
+      title: 'Transactions',
+      value: '$000M+'
+   }
+];
+
+export const footerLinks = [
+   {
+      title: 'Useful Links',
+      links: [
+         {
+            name: 'Content',
+            link: 'https://www.hoobank.com/content/'
+         },
+         {
+            name: 'How it Works',
+            link: 'https://www.hoobank.com/how-it-works/'
+         },
+         {
+            name: 'Create',
+            link: 'https://www.hoobank.com/create/'
+         },
+         {
+            name: 'Explore',
+            link: 'https://www.hoobank.com/explore/'
+         },
+         {
+            name: 'Terms & Services',
+            link: 'https://www.hoobank.com/terms-and-services/'
+         }
+      ]
+   },
+   {
+      title: 'Community',
+      links: [
+         {
+            name: 'Help Center',
+            link: 'https://www.hoobank.com/help-center/'
+         },
+         {
+            name: 'Partners',
+            link: 'https://www.hoobank.com/partners/'
+         },
+         {
+            name: 'Suggestions',
+            link: 'https://www.hoobank.com/suggestions/'
+         },
+         {
+            name: 'Blog',
+            link: 'https://www.hoobank.com/blog/'
+         },
+         {
+            name: 'Newsletters',
+            link: 'https://www.hoobank.com/newsletters/'
+         }
+      ]
+   },
+   {
+      title: 'Partner',
+      links: [
+         {
+            name: 'Our Partner',
+            link: 'https://www.hoobank.com/our-partner/'
+         },
+         {
+            name: 'Become a Partner',
+            link: 'https://www.hoobank.com/become-a-partner/'
+         }
+      ]
+   }
+];
+
+export const socialMedia = [
+   {
+      id: 'social-media-1',
+      icon: instagram,
+      link: 'https://www.instagram.com/'
+   },
+   {
+      id: 'social-media-2',
+      icon: facebook,
+      link: 'https://www.facebook.com/'
+   },
+   {
+      id: 'social-media-3',
+      icon: twitter,
+      link: 'https://www.twitter.com/'
+   },
+   {
+      id: 'social-media-4',
+      icon: linkedin,
+      link: 'https://www.linkedin.com/'
+   }
+];
+
+export const clients = [
+   {
+      id: 'client-1',
+      logo: airbnb
+   },
+   {
+      id: 'client-2',
+      logo: binance
+   },
+   {
+      id: 'client-3',
+      logo: coinbase
+   },
+   {
+      id: 'client-4',
+      logo: dropbox
+   }
+];
